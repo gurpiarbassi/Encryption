f2382b0a83aa66e6569152e9d3443a7471554eeb
Gurps Bassi <gurpiar.bassi@gmail.com>
2014-09-03 15:34:51 -0400

    mavenised project. Also added decryption functionality. used bidimap to allow reverse lookup of key from value.

--- Encryption/src/com/gurps/encryption/cipher/SubstitutionCipher.java
+++ Encryption/src/com/gurps/encryption/cipher/SubstitutionCipher.java
@@ -1,28 +1,33 @@
 package com.gurps.encryption.cipher;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.Arrays;
+
+import org.apache.commons.collections.BidiMap;
+import org.apache.commons.collections.bidimap.DualHashBidiMap;
 
 /**
  * Very basic substitution cipher that simply replaces each character in a string with another
- * String. If the character is not found then it substitutes a '*91' instead.
+ * String.
+ * 
+ * At the moment this Cipher only supports messages with characters A-Z, space and period (.)
+ * If a unkown character is found, it is replaced by empty string.
  * 
  * @author gurps.bassi
  *
  */
 public class SubstitutionCipher {
 
-  private static Map <String, String> substitutionMap = new HashMap<>();
+  private static BidiMap substitutionMap = new DualHashBidiMap();
   
   static {
-    substitutionMap.put("A", "2@");
-    substitutionMap.put("B", "3#");
-    substitutionMap.put("C", "4$");
-    substitutionMap.put("D", "5%");
-    substitutionMap.put("E", "6^");
-    substitutionMap.put("F", "7&");
-    substitutionMap.put("G", "8*");
-    substitutionMap.put("H", "9(");
+    substitutionMap.put("A", "2@@");
+    substitutionMap.put("B", "3##");
+    substitutionMap.put("C", "4$$");
+    substitutionMap.put("D", "5%%");
+    substitutionMap.put("E", "6^^");
+    substitutionMap.put("F", "7&&");
+    substitutionMap.put("G", "8**");
+    substitutionMap.put("H", "9((");
     substitutionMap.put("I", "10)");
     substitutionMap.put("J", "11!");
     substitutionMap.put("K", "12@");
@@ -41,19 +46,29 @@ public class SubstitutionCipher {
     substitutionMap.put("X", "25%");
     substitutionMap.put("Y", "26^");
     substitutionMap.put("Z", "27&");
+    substitutionMap.put(" ", "*91");
+    substitutionMap.put(".", "*92");
 }
       
-  public String encryptChar(char c){
-    String cipherChar = substitutionMap.get((c + "").toUpperCase());
+  private String encryptChar(final char c){
+    String cipherChar = (String)substitutionMap.get((c + "").toUpperCase());
     if(cipherChar == null){
-      //default to *91.
-      cipherChar = "*91";
+      cipherChar = "";
     }
     
     return cipherChar;
   }
   
-  public String encrypt(String message){
+  private String decryptSubstitutetChar(final String encrypted){
+    String plainTextChar = (String)substitutionMap.getKey(encrypted);
+    if(plainTextChar == null){
+      plainTextChar = "*";
+    }
+    
+    return plainTextChar;
+  }
+  
+  public String encrypt(final String message){
     if(message == null || message.length() == 0){
       return "";
     }
@@ -61,6 +76,16 @@ public class SubstitutionCipher {
     StringBuilder output = new StringBuilder();
     
     message.chars().forEach(i -> output.append(encryptChar((char)i)));
+    return output.toString();
+  }
+  
+  public String decrypt(final String encrypted){
+    
+    StringBuilder output = new StringBuilder();
+    
+    //All substitutes are of length 3 - so regex splits in groups of 3.
+    String[] substitutes = encrypted.split("(?<=\\G.{3})");
+    Arrays.stream(substitutes).forEach(i -> output.append(decryptSubstitutetChar(i)));
     return output.toString();
   }
 }
--- Encryption/test/src/com/gurps/encryption/cipher/TestSubstitutionCipher.java
+++ Encryption/test/src/com/gurps/encryption/cipher/TestSubstitutionCipher.java
@@ -7,12 +7,21 @@ import org.junit.Test;
 public class TestSubstitutionCipher {
 
   @Test
-  public void test() {
-    final String message = "Hello World!";
+  public void testEncrypt() {
+    final String message = "Hello World.";
     SubstitutionCipher cipher = new SubstitutionCipher();
     String encryptedMessage = cipher.encrypt(message);
     
-    assertEquals("9(6^13#13#16^*9124$16^19(13#5%*91", encryptedMessage);
+    assertEquals("9((6^^13#13#16^*9124$16^19(13#5%%*92", encryptedMessage);
+  }
+  
+  @Test
+  public void testDecrypt() {
+    final String encryptedMessage = "9((6^^13#13#16^*9124$16^19(13#5%%*92";
+    SubstitutionCipher cipher = new SubstitutionCipher();
+    String decryptedMessage = cipher.decrypt(encryptedMessage);
+    
+    assertEquals("HELLO WORLD.", decryptedMessage);
   }
 
 }
